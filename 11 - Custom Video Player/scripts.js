443b0d8afcc874eaba0e5dd39656d31128e3c45e
woodj22 <joe.wood@bbc.co.uk>
2017-03-06 21:35:43 +0000

    constants added and video is playing on toggle. some event listeners are listening.

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,35 @@
+
+
+
+// get elements
+
+//build our functions
+
+// hook up event listeners
+
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+
+function togglePlay(){
+
+    const method = video.paused ? 'play' : 'pause';
+   video[method]();
+
+}
+function updateButton(){
+    console.log('update the button');
+}
+video.addEventListener('click', togglePlay);
+video .addEventListener('play', updateButton);
+video .addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+
